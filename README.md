dfae40b36572d0d9dd3cf725defb2c361f777b77
guestl <boris.nikulin@gmail.com>
2017-10-13 23:36:39 +0300

    The first release

--- README.md
+++ README.md
@@ -1,6 +1,30 @@
 # Password Strength Calculator
 
-[TODO. There will be project description]
+The script counts password strength and print result in console
+
+0 - the weakest password
+10 - the strongest password
+
+It counts the password strength according to password lenght, special symbols, digits or upper- low- case characters using.
+
+# Quickstart
+
+Example of script launch on Linux, Python 3.5:
+
+```#!bash
+
+python password_strength.py qwrtyu43
+The password strength is 7 in points from 0 to 10
+
+python password_strength.py qwerty
+The password strength is 4 in points from 0 to 10
+
+python password_strength.py qwerty#!!1
+The password strength is 9 in points from 0 to 10
+
+python password_strength.py qwerty#!!1Y
+The password strength is 10 in points from 0 to 10
+```
 
 # Project Goals
 
--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,47 @@
+import argparse
 
 
 def get_password_strength(password):
-    pass
+    max_strength = 10
+    normal_value = 1
+
+    minimal_length = 10
+    digits = '1234567890'
+    special_chars = '[]\;,"./~!@#$%^&*()_+{|}:\'<>?'
+
+    upcase_counter = max(
+        sum(1 for single_char in password if single_char.isupper()), 0.1)
+    locase_counter = max(
+        sum(1 for single_char in password if single_char.islower()), 0.1)
+    digits_counter = sum(
+        1 for single_char in password if single_char in digits)
+    spec_counter = sum(
+        1 for single_char in password if single_char in special_chars)
+
+    digits_strength = min(digits_counter / len(password), normal_value)
+
+    len_strength = min (normal_value, len(password) / minimal_length)
+
+    lowup_case_strength = min(min((upcase_counter / locase_counter),
+                                  (locase_counter / upcase_counter)),
+                              normal_value)
+
+    spec_strength = spec_counter / len(password)
+
+    strength_list = [len_strength, lowup_case_strength,
+                     digits_strength, spec_strength]
+
+    return round(sum(strength_list) / len(strength_list) *
+                 max_strength * max_strength / len(strength_list))
 
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser(
+        description='The script calculates password strenth.')
+    parser.add_argument('password', type=str,
+                        help='password to check strength.')
+
+    args = parser.parse_args()
+    password_string = args.password
+
+    print("The password strength is {} in points from 0 to 10".format(get_password_strength(password_string)))
